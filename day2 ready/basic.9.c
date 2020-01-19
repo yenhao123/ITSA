f08522ff88a96d1e1498b7db2d095997f9b6e284
yenhao123 <46972327+yenhao123@users.noreply.github.com>
2020-01-20 03:08:58 +0800

    Update basic.9.c

--- day2 ready/basic.9.c
+++ day2 ready/basic.9.c
@@ -10,10 +10,13 @@
 int main(){
     int num,total=0;
     while(scanf("%d",&num)!=EOF){
+        //initial
         total=0;
+        //determine
         for(int i=1;i<=num;i++){
             if(i%3==0)total+=i;
         }
+        //output
         printf("%d\n",total);
     }
 }
