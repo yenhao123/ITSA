2310ac892211c2335988491c71df8801abbb824f
yenhao123 <46972327+yenhao123@users.noreply.github.com>
2020-01-20 01:04:18 +0800

    Add files via upload

---
+++ day2 ready/MM01(mathmetic-1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#define TRUE 1
+#define FALSE 0
+
+int main(){
+    int h1,h2,h3;
+    while(scanf("%d %d %d",&h1,&h2,&h3)!=EOF){
+        printf("Trapezoid area:%.1f\n",(float)(h1+h2)*h3/2);
+    }
+}
---
+++ day2 ready/mm-03(methmetic-1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#define TRUE 1
+#define FALSE 0
+
+int main(){
+    int a,b;
+    while(scanf("%d %d",&a,&b)!=EOF){
+        printf("%d\n",a+b);
+    }
+}
