5fdfdff310e4198b1703d22b58a69735bbf99b68
yenhao123 <46972327+yenhao123@users.noreply.github.com>
2020-01-20 03:07:07 +0800

    Update basic-14.c

--- day2 ready/basic-14.c
+++ day2 ready/basic-14.c
@@ -11,12 +11,14 @@ int main(){
     char word[100];
     int i,j,flag,len,num[100];
     while(scanf("%s",word)!=EOF){
+        //initial
         flag=TRUE;
         len=strlen(word);
-        //determine
+        //determine 前面與後面相不相同
         for(i=0;i<len/2;i++){
             if(word[i]!=word[len-i-1]){flag=FALSE;break;}
         }
+        //output
         if(flag==FALSE)printf("NO\n");
         else printf("YES\n");
     }
