4f1c95f24cf79c018d1e89e09570937c8094e451
yenhao123 <46972327+yenhao123@users.noreply.github.com>
2020-01-20 02:57:15 +0800

    Update c_ar73(ar-2).c

--- day2 ready/c_ar73(ar-2).c
+++ day2 ready/c_ar73(ar-2).c
@@ -8,7 +8,7 @@
 //in 陣列2 c_ar73
 
 int total[100];
-//要列出每一項兔子數列 所以不使用遞迴
+//列出每一項兔子數列 所以不使用遞迴
 int func(int n){
     total[0]=1;total[1]=1;
     for(int i=2;i<n;i++){
@@ -21,12 +21,16 @@ int main(){
     total[0]=1;total[1]=1;
     scanf("%d",&num);
     for(i=0;i<num;i++){
+        //input
         scanf("%d",&n);
+        //兔子數列
         func(n);
+        //output
         for(j=0;j<n;j++){
             if(j==n-1)printf("%d\n",total[j]);
             else printf("%d ",total[j]);
         }
+        //清空
         for(j=0;j<n;j++)total[j]=0;
     }
 }
